ca50bf0743adbef0591b268ed80a6569cfc88140
nadia <crocodile.who.wears.socks@yandex.ru>
2022-03-15 17:23:20 +0300

    fixed

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import logging
-
+import os
 import json
 
 app = Flask(__name__)
@@ -71,4 +71,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
